73f9ddf01033185a67156467086b858c0ad1549f
cpadilla <christoferpadilla@gmail.com>
2018-03-15 23:55:51 -0400

    Pastebin call now requests link from Pastebin API

--- routes/pastebin.js
+++ routes/pastebin.js
@@ -1,8 +1,25 @@
+var PastebinAPI = require('pastebin-js')
+
 exports.getLink = function(req, res, next) {
     console.log('getlink POST recieved')
 
-    // TODO make request to pastebin api
-    res.send({
-        link: "https://pastebin.com"
-    });
+    // TODO move key to a config
+    var pastebin = new PastebinAPI('c2782b965a1788ff8fd954b3f30d8b52')
+
+    // Requet Pastebin link from API
+    pastebin.createPaste(req.body.pasteString, "", req.body.syntaxHighlighting, 0)
+    .then(function(data) {
+        console.log(data)
+
+        res.send({
+            link: data + ""
+        });
+    })
+    .fail(function(err) {
+        console.log(err)
+
+        res.send({
+            link: "error"
+        });
+    })
 }
